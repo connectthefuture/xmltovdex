e8fbd332462ec1881d1b420072bfd5f1dd590eef
Andre Goncalves <andreslb1@gmail.com>
2016-12-15 18:00:54 +0100

    Convert entire XML ready

--- xmltovdex.py
+++ xmltovdex.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-""" Script that converts an Adlib XML (Taxonomy) into VDEX 
+""" 
+    Script that converts an Adlib XML (Taxonomy) into VDEX 
     Andre Goncalves <andre@intk.com>
 """
 
@@ -13,6 +14,8 @@ import datetime
 XML_PATH = "Taxonomies-v02.xml"
 EXPORT_FILE_PATH = "taxonomies-%s.vdex"
 
+debug = True
+testing = False
 
 class XMLtoVDEX:
     def __init__(self, vocabulary_name, vocabulary_identifier):
@@ -26,6 +29,16 @@ class XMLtoVDEX:
 
         date_now = datetime.datetime.now().strftime("%Y-%m-%d-%H:%M:%S") 
         self.file_name = EXPORT_FILE_PATH %(date_now)
+
+        self.taxonomy_rank = ["kingdom", "subkingdom", "phylum/division", "subphylum/subdivision", "superclass", "class", "subclass", "infraclass", "superorder", "order", "suborder", "infraorder", "superfamily", "family", "subfamily", "tribe", "subtribe", "genus", "subgenus", "section", "subsection", "species", "subspecies", "variety", "subvariety", "form", "subform", ""]
+        self.created_notes = []
+        self.ranks = {}
+
+    def log(self, text):
+        if debug:
+            print text
+        else:
+            pass
 
     def addSubElement(self, parent, elementName, text=None):
         retval = etree.SubElement(parent, self.vdexmanager.vdexTag(elementName))
@@ -54,14 +67,12 @@ class XMLtoVDEX:
         details["name"] = record.find('scientific_name').text
         
         # create_term in that level
-        print "PARENT PRIREF: %s" %(parent_priref)
         if parent_priref != 0:
             parent = self.vdexmanager.getTermById(parent_priref)
         else:
             parent = self.vdexmanager.tree.getroot()
 
         if not parent:
-            print "NOT PARENT"
             parent = self.vdexmanager.tree.getroot()
         
         term = self.addSubElement(parent, "term")
@@ -80,26 +91,87 @@ class XMLtoVDEX:
         
         # Go through all the childs
         # Create node from parent_priref
-        if parent_priref == None:
-            self.create_node(0, record)
-        else:
-            self.create_node(parent_priref, record)
+        if priref not in self.created_notes:
+            if parent_priref == None:
+                self.create_node(0, record)
+            else:
+                self.create_node(parent_priref, record)
 
-        children = record.findall('Child')
-        if len(children):
-            for child in children:
-                if child.find('child_name'):
-                    child_priref = child.find('child_name').get('priref')
-                    self.build_tree_from_priref(priref, child_priref)
+            children = record.findall('Child')
+            if len(children):
+                for child in children:
+                    if child.find('child_name'):
+                        child_priref = child.find('child_name').get('priref')
+                        self.build_tree_from_priref(priref, child_priref)
+
+            self.created_notes.append(priref)
+        else:
+            pass
 
         return result
 
     def test_run(self):
         test_priref = "8703" # Amphibia
-        print "isVDEX: %s" %(self.vdexmanager.isVDEX())
-        print "Number of records: %s" %(len(self.records))
+        self.log("isVDEX: %s" %(self.vdexmanager.isVDEX()))
+        self.log("Number of records: %s" %(len(self.records)))
         self.build_tree_from_priref(None, test_priref)
-        print self.vdexmanager.serialize()
+        self.log(self.vdexmanager.serialize())
+
+
+    def get_records_by_rank(self, rank):
+        results = []
+
+        if rank != "":
+            for record in self.records:
+                for record_rank in record.findall('rank'):
+                    if record_rank.find('text') != None:
+                        r = record_rank.find('text').text
+                        if r == rank:
+                            results.append(record)
+        else:
+            for record in self.records:
+                if not len(record.findall('rank')):
+                    results.append(record)
+
+            rank = "No rank available"
+        return results
+
+
+    def run_converter(self):
+        self.log("isVDEX: %s" %(self.vdexmanager.isVDEX()))
+        self.log("Number of records: %s" %(len(self.records)))
+
+        total_ranks = len(self.taxonomy_rank)
+        curr = 0
+        for rank in self.taxonomy_rank:
+            curr += 1
+            self.log("\n[%s] Converting rank %s / %s" %(rank, curr, total_ranks))
+            
+
+            results = self.get_records_by_rank(rank)
+            curr_term = 0
+            total_terms = len(results)
+            self.log("[%s] Total terms: %s" %(rank, total_terms))
+            if rank != "":
+                self.ranks[rank] = total_terms
+            else:
+                self.ranks['No available rank'] = total_terms
+
+            for record in results:
+                curr_term += 1
+                self.log("[%s] Converting term %s / %s" %(rank, curr_term, total_terms))
+            
+                record_priref = record.find('priref').text
+                self.build_tree_from_priref(None, record_priref)
+
+
+        self.vdex_file = open(self.file_name, "w")
+        xml_serialize = self.vdexmanager.serialize()
+        self.vdex_file.write(xml_serialize)
+        self.log("Converter finished!")
+        self.log("Total terms created: %s\nTotal records: %s\n" %(len(self.created_notes), len(self.records)))
+        self.log(self.ranks)
+        return True
         
 
     def create_vdex_structure(self):
@@ -127,7 +199,11 @@ if __name__ == "__main__":
     vocabulary_identifier = "VocabularyIdentifier"
     xmltovdex = XMLtoVDEX(vocabulary_name, vocabulary_identifier)
     xmltovdex.init()
-    xmltovdex.test_run()
+    if testing:
+        xmltovdex.test_run()
+    else:
+        xmltovdex.run_converter()
+
 
 
 
